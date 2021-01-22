e5670f99a245d97ea2394121d615196309ce9a25
rawanebeidat <r.ebeidat@hotmail.com>
2021-01-22 00:48:34 +0000

    add requirements

--- run.py
+++ run.py
@@ -3,8 +3,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "<h1>Hello there!</h1>"
 
-    app.run(host=os.getenv('IP'), port=int(os.getenv('PORT')), debug=True)
+@app.route("/")
+def index():
+    return "<h1>Hello There!</h1>"
+
+
+if __name__ =="__main__":
+    app.run(host=os.getenv("IP"),
+       port=int(os.getenv("PORT")),
+       debug=True)
